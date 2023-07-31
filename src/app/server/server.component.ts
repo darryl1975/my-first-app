7c7eec6fce7c1d22bdafe9fab494fa3a1da1acff
Darryl <darryl1975@hotmail.com>
2023-07-31 16:04:32 +0800

    first commit

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,24 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html',
+    styleUrls: ['./server.component.css']
+})
+export class ServerComponent {
+    serverId : number = 11;
+    serverStatus : string = 'online';
+
+    constructor() {
+        this.serverId = Math.floor(Math.random() * 10);
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    }
+
+    getServerStatus() {
+        return this.serverStatus;
+    }
+
+    getColor() {
+        return this.serverStatus === 'online' ? 'green' : 'red';
+    }
+}
